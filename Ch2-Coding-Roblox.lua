6c4de7d2b7e18d040fb7ac633b2d805ae5cf4110
Gravity Online <albertoborgato9@gmail.com>
2025-03-23 22:20:15 +0100

    Create Ch2-Coding-Roblox.lua

---
+++ Ch2-Coding-Roblox.lua
@@ -0,0 +1,38 @@
+Chapter 2: Knowing Your Work
+Environment
+
+Technical requirements
+
+Traversing the Create page
+Configuring Game and Place
+settings
+The Configure Game menu
+Configuring Start Place menu
+Now, if
+  IMPORTANT NOTE
+  Other Place options
+  IMPORTANT NOTE
+  The Library and Avatar Shop
+  IMPORTANT NOTE
+  Getting Started with Roblox
+studio
+  The File menu and settings
+  Movement and camera
+manipulation
+  Utilizing the Explorer
+  Using Studio tools
+  Managing the Game Settings
+menu
+  IMPORTANT NOTE
+  The View tab
+  The Test tab
+  Customizing Studio to aid your
+workflow
+  Taking advantage of Roblox's
+resources
+  Tutorials and resources
+  The Developer Forum
+  Summary
+  Section 2: Programming in
+Roblox
+  
