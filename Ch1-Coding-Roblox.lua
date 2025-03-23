d40ed8d83bb7604ad6627fdf38c25685791a76fa
Gravity Online <albertoborgato9@gmail.com>
2025-03-23 20:54:36 +0100

    Create Ch1-Coding-Roblox.lua

---
+++ Ch1-Coding-Roblox.lua
@@ -0,0 +1,31 @@
+Who this book is for
+What this book covers
+To get the most out of this book
+Download the example code files
+Download the color images
+Conventions used
+TIPS OR IMPORTANT NOTES
+Get in touch
+Reviews
+Section 1: Introduction to
+Roblox Development
+Chapter 1: Introducing Roblox
+Development
+Technical requirements
+Learning the benefits of Roblox
+development
+Financial opportunities on Roblox
+IMPORTANT NOTE
+Improving professional skills
+Benefits of networking
+Discovering developer types
+Programmers
+Modelers
+Builders
+UI/UX designers
+Gaining a perspective about
+your early projects
+Summary
+
+Chapter 2: Knowing Your Work
+Environment
